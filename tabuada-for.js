36fb46cbebfea62925ae7c25efbb6b11cda937ed
Daniel-TI <daniel.silveira@indbandeirante.com.br>
2022-05-02 16:07:30 -0300

    função de tabuadas

---
+++ tabuada-for.js
@@ -0,0 +1,12 @@
+let numeroDigitado = 3;
+
+for (let i = 0; i < 11; i++) {
+    
+    console.log (`Tabuada do ` + i)
+
+    for (let j = 0; j < 11; j++) {
+
+        console.log (numeroDigitado + ' x ' + i + ' = ' + (i * j))
+    }
+
+}
---
+++ tabuada_map.js
@@ -0,0 +1,6 @@
+let listaDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const funcaoTabuada = listaDeNumeros.map (num => num * 2);
+
+console.log(funcaoTabuada === listaDeNumeros);
+console.log(funcaoTabuada);
